e6e7bdd758956d0866926ba432a05c6fcb30f561
YunSeong Park <75507166+YunSeong-Park@users.noreply.github.com>
2021-07-20 15:45:03 +0900

    [New Feature, init] 프로젝트 시작
    https://www.youtube.com/watch?v=4sO0aWTd3yc&list=WL&index=70 의
    함수형 프로그래밍 연습 시작

---
+++ index.js
@@ -0,0 +1,8 @@
+{
+    var log = console.log;
+    function f(list, length) {
+    }
+    function main() {
+        f([1, 2, 3, 4, 5], 2);
+    }
+}
---
+++ index.ts
@@ -0,0 +1,12 @@
+{
+  const log = console.log 
+  
+  function f(list:number[], length:number) {
+    
+    
+  }
+
+  function main() {
+    f([1, 2, 3, 4, 5], 2);
+  }
+}
