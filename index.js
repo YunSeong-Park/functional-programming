ad8c031824b94426ef841f55c1804cbb6d92cf4e
YunSeong Park <75507166+YunSeong-Park@users.noreply.github.com>
2021-07-20 15:52:36 +0900

    [New Feature, normal] 일반적인 홀수 제곱 구하기

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 {
-    var log = console.log;
+    var log_1 = console.log;
     function f(list, length) {
+        var i = 0;
+        var acc = 0;
+        for (var _i = 0, list_1 = list; _i < list_1.length; _i++) {
+            var a = list_1[_i];
+            if (a % 2) {
+                acc = acc + a * a;
+                if (++i === length) {
+                    break;
+                }
+            }
+        }
+        log_1(acc);
     }
     function main() {
+        f([1, 2, 3, 4, 5], 1);
         f([1, 2, 3, 4, 5], 2);
+        f([1, 2, 3, 4, 5], 3);
     }
+    main();
 }
--- index.ts
+++ index.ts
@@ -2,11 +2,23 @@
   const log = console.log 
   
   function f(list:number[], length:number) {
-    
-    
+    let i = 0;
+    let acc = 0;
+    for (const a of list){
+      if( a % 2) {
+        acc = acc + a * a;
+        if(++i === length){
+          break
+        }
+      }
+    }
+    log(acc)
   }
 
   function main() {
+    f([1, 2, 3, 4, 5], 1);
     f([1, 2, 3, 4, 5], 2);
+    f([1, 2, 3, 4, 5], 3);
   }
+  main();
 }
