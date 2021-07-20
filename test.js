ee5fe8619683e9e4ea7ed71d775617d55371d378
YunSeong Park <75507166+YunSeong-Park@users.noreply.github.com>
2021-07-20 16:37:35 +0900

    [Style/ functional] 명령형 코드에서 선언형으로코드 변경

--- test.js
+++ test.js
@@ -2,30 +2,52 @@
   const log = console.log 
 
 
-  function* filter(f, list) {
-    for (const a of list){
+  function *filter(f, iter) {
+    for (const a of iter){
       if (f(a)) {
         yield a;
       }
     }
   }
 
-  function f(list, length) {
-    let i = 0;
-    let acc = 0;
-    for (const a of filter((a) => a % 2, list)){
-      acc = acc + a * a;
-      if(++i === length){
-        break
-      }
+  function *map(f, iter) {
+    for (const a of iter){
+      yield f(a);
     }
-    log(acc)
   }
 
+  function take(length, iter) {
+    let res = [];
+    for (const a of iter) {
+      res.push(a);
+      if (res.length == length){
+        return res
+      }
+    }
+    return res
+  }
+
+  function reduce(f, acc, iter) {
+    for (const a of iter) {
+      acc = f(acc, a);
+    }
+    return acc;
+  }
+
+  const add = (a, b) => a + b;
+
+
+  const f = (list, length) =>  
+    reduce(
+      add, 
+      0, 
+      take(length, map(a=> a*a ,filter((a) => a % 2, list))))
+  
+
   function main() {
-    f([1, 2, 3, 4, 5], 1);
-    f([1, 2, 3, 4, 5], 2);
-    f([1, 2, 3, 4, 5], 3);
+    log(f([1, 2, 3, 4, 5], 1));
+    log(f([1, 2, 3, 4, 5], 2));
+    log(f([1, 2, 3, 4, 5], 3));
   }
   main();
   
